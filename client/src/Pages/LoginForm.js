19aad674e1593c6d09a93bb4b8a02e4cbd544675
keerthirevuru <keerthi@rekonsys.tech>
2024-04-24 12:25:13 +0530

    link added

--- client/src/Pages/LoginForm.js
+++ client/src/Pages/LoginForm.js
@@ -59,7 +59,7 @@ const handleSubmit = async (e) => {
                             <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
-                      <a href="/forgotpassword" className="text-sm font-medium text-blue-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <Link to="/forgotpassword" className="text-sm font-medium text-blue-600 hover:underline dark:text-primary-500">Forgot password?</Link>
                   </div>
                   <button  className="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
